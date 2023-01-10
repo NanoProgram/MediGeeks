a975bb0d206f5377cb998def292b7105fbda1382
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-01-10 01:38:57 +0000

    login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,17 +1,18 @@
 
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
   export function Login () {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+   
+    <Container className='text-align: center'>
+    <Form  >
+      <Form.Group className="mb-3 " controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -21,10 +22,16 @@ import Form from 'react-bootstrap/Form';
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button variant="secondary" type="submit">
+        Sign whit Gmail
+      </Button>  
+      &nbsp;&nbsp;
+      <Button variant="secondary" type="submit">
+        Sign in
       </Button>
     </Form>
+        </Container>
+    
   );
-}
+};
 
