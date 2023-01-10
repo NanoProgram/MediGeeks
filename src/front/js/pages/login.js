0ad108bad9dc38ee3f36e2ad13458e5b5f09f3d6
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-01-10 14:09:41 +0000

    casi casi

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,5 +1,4 @@
-
-import React from 'react';
+/*import React from 'react';
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -34,4 +33,86 @@ import Form from 'react-bootstrap/Form';
     
   );
 };
+*/
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+  Image,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
+export function Login() {
+  return (
+    <Container fluid>
+      <div
+        className="p-5"
+        style={{
+          backgroundImage:
+            "url(https://mdbootstrap.com/img/new/textures/full/171.jpg)",
+          height: "300px",
+        }}
+      ></div>
+
+      <Card
+        className="mx-5 mb-5 p-5 shadow-5"
+        style={{
+          marginTop: "-100px",
+          background: "hsla(0, 0%, 100%, 0.8)",
+          backdropFilter: "blur(30px)",
+        }}
+      >
+        <Card.Body className="p-5 text-center">
+          <h1 className="fw-bold mb-5">MediGeeks</h1>
+          <Row>
+            <Col xs={6}>
+              <Form.Control placeholder="First name" id="form1" type="text" />
+            </Col>
+            <Col xs={6}>
+              <Form.Group>
+                <Form.Control placeholder="Last name" id="form1" type="text" />
+              </Form.Group>
+            </Col>
+            &nbsp;
+          </Row>
+          <Form.Control
+            wrapperClass="mb-4"
+            placeholder="Email"
+            id="form1"
+            type="email"
+          />
+          &nbsp;
+          <Form.Control
+            wrapperClass="mb-4"
+            placeholder="Password"
+            id="form1"
+            type="password"
+          />
+          &nbsp;
+          <Button className="w-100 mb-4" size="md">
+            sign up
+          </Button>
+          <div className="text-center">
+            <p>or sign up with:</p>
+
+            <Button
+              className="mx-3"
+              variant="light"
+              style={{
+                boxShadow: "2px 2px 3px #ccc",
+                border: "1px solid #ccc",
+              }}
+            >
+              <FontAwesomeIcon icon={faGoogle} size="sm" />
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
