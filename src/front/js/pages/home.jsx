65e574a2b7c48eb150ccdde86bafcae4696c54ec
Juliuuss <j.esp.ver@gmail.com>
2023-01-13 19:07:09 +0000

    primer cambio

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -2,20 +2,29 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserDoctor } from "@fortawesome/free-solid-svg-icons";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="container text-center border p-1">
+    <div className="container text-center border p-1 ">
       <div className="text-center mt-5">
         <h1>MediGeeks GO GO GO</h1>
       </div>
-      <div className="row justify-content-center g-2 ">
+      <div className="row justify-content-center g-2 w-75 ">
         <div className="col-4 ">
-          <div className="border border-dark p-2" id="esp_11">
-            <p>espacio 1</p>
-          </div>
+          <Link to="/cita">
+            <div className="border border-dark p-2" id="esp_11">
+              <FontAwesomeIcon icon={faUserDoctor} size="5x" />
+              <h4>Reserva de Hora</h4>
+            </div>
+          </Link>
         </div>
         <div className="col-4">
           <div className="border border-dark p-2" id="esp_12">
