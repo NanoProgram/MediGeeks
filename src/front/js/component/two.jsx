08e2183dba22e6f5a7128d40d36b8ac488de3fbc
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-01-20 17:09:10 +0000

    creando

--- src/front/js/component/two.jsx
+++ src/front/js/component/two.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
+import "../../styles/two.css"
+
 
 export const Two = () => {
     return (
         <div class="card-group d-flex justify-content-center"  >
             <div class="card">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAMDeyo7haKSUnnQGoUP5IUOfG673bkGYnAel0VQRtdycAeWju-s7Sr3KKD9EZbvr22cs&usqp=CAU" class="card-img-top " alt="..." ></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAMDeyo7haKSUnnQGoUP5IUOfG673bkGYnAel0VQRtdycAeWju-s7Sr3KKD9EZbvr22cs&usqp=CAU" class="card-img-top separated-img " alt="..." ></img>
                 <div class="card-body">
                     <h5 class="card-title">Registrate en nuestra plataforma</h5>
                     <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
             </div>
+            
             <div class="card">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAMDeyo7haKSUnnQGoUP5IUOfG673bkGYnAel0VQRtdycAeWju-s7Sr3KKD9EZbvr22cs&usqp=CAU" class="card-img-top" alt="..."></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAMDeyo7haKSUnnQGoUP5IUOfG673bkGYnAel0VQRtdycAeWju-s7Sr3KKD9EZbvr22cs&usqp=CAU" class="card-img-top separated-img" alt="..."></img>
                 <div class="card-body">
                     <h5 class="card-title">Agenda tu hora</h5>
                     <p class="card-text text-black">This card has supporting text below as a natural lead-in to additional content.</p>
                 </div>
             </div>
+            
             <div class="card">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAMDeyo7haKSUnnQGoUP5IUOfG673bkGYnAel0VQRtdycAeWju-s7Sr3KKD9EZbvr22cs&usqp=CAU" class="card-img-top" alt="..." ></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAMDeyo7haKSUnnQGoUP5IUOfG673bkGYnAel0VQRtdycAeWju-s7Sr3KKD9EZbvr22cs&usqp=CAU" class="card-img-top separated-img" alt="..." ></img>
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
