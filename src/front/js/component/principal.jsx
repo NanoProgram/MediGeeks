af7b2b93a0cabeb49bb334eb6a786861b8075ea1
Juliuuss <j.esp.ver@gmail.com>
2023-01-20 22:02:15 +0000

    cambios

--- src/front/js/component/principal.jsx
+++ src/front/js/component/principal.jsx
@@ -1,39 +1,34 @@
-import React from 'react'
+import React from "react";
 import { Inicio } from "../component/beginning.jsx";
 import { One } from "../component/one.jsx";
 import { Two } from "../component/two.jsx";
 import { Three } from "../component/three.jsx";
 import { Four } from "../component/four.jsx";
 import { Five } from "../component/five.jsx";
-import {Footer} from "../component/footer.jsx"
-
-import "../../styles/principal.css"
+import "../../styles/principal.css";
 
 export const Principal = () => {
   return (
-    <div className=' background conteiner-md' >
-        
-        <Inicio />
-        <br />
-        <br />
-        <br />
-        <One />
-        <br />
-        <br />
-        <br />
-        <Two />
-        <br />
-        <br />
-        <br />
-        <Three />
-        <br />
-        <br />
-        <br />
-        <Four />
-        <br />
-        <Five />
-        <br />
-        <Footer />
+    <div className=" background container-md">
+      <Inicio />
+      <br />
+      <br />
+      <br />
+      <One />
+      <br />
+      <br />
+      <br />
+      <Two />
+      <br />
+      <br />
+      <br />
+      <Three />
+      <br />
+      <br />
+      <br />
+      <Four />
+      <br />
+      <Five />
     </div>
-  )
-}
+  );
+};
