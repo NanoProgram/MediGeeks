773b33ccf026bf0eadd4d26a76c937d012db08cf
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-01-21 16:14:25 +0000

    emailjs LISTOgit add .!

---
+++ src/front/js/service/emailService.js
@@ -0,0 +1,22 @@
+import emailjs from "@emailjs/browser";
+
+export const sendEmail = (params) => {
+  const serviceId = process.env.SERVICE_ID;
+  const templateId = process.env.TEMPLATE_ID;
+  const key = process.env.KEY;
+console.log(params)
+  const templateParams = {
+    from_name: "MediGeeks",
+    to_email: params.to_email,
+    to_name: params.to_name,
+    to_link: params.to_link
+  };
+  emailjs.send(serviceId, templateId, templateParams, key).then(
+    (result) => {
+      console.log(result.text);
+    },
+    (error) => {
+      console.log(error.text);
+    }
+  );
+};
