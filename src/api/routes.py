9b35eb136716e36edaccb8973bcde4528530558b
NanoProgram <danielj.perezd@gmail.com>
2023-01-22 00:02:49 +0000

    API DOC, USER, SPECIALITY, CENTER and PREVISION ready

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,18 +4,18 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Prevision, Speciality, Center, Doctor
 from api.utils import generate_sitemap, APIException
+import re
 
 api = Blueprint('api', __name__)
 
 
-#API USER GET
+#API USER GET, GET ID and POST
 @api.route('/mediGeeks/users', methods=['GET'])
 def get_users_table():
     user = User.query.all()
     user = list(map(lambda p:p.serialize(),user))
     return jsonify(user), 200 
 
-#API USER GET/ID
 @api.route('/mediGeeks/users/<int:id>', methods=['GET'])
 def get_users_table_id(id):
     user = User.query.filter_by(id=id).first()
@@ -24,22 +24,68 @@ def get_users_table_id(id):
     else:
         return "Usuario no existe", 404
 
-
-#API users POST
 @api.route('/mediGeeks/users', methods=['POST'])
 def add_new_user():
     request_body = request.get_json()
-    new_user = User(**request_body)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(new_user.serialize()), 201
+    name = request_body.get("name")
+    email = request_body.get("email")
+    rut = request_body.get("rut")
+    password = request_body.get("password")
+    errors = {}
+    if not name or not re.match(r"^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]+(?: [a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]+)*$", name):
+        errors["name"] = "Name should only contain letters, spaces and some accented characters"
+    if not rut or not re.match(r"\b[0-9|.]{1,10}\-[K|k|0-9]", rut):
+        errors["rut"] = "Rut no es valido"
+    if not email or not re.match(r"^(([^<>()[\],;:\s@']+(\.[^<>()[\],;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", email):
+        errors["email"] = "Invalid email address"
+    if not password or not re.match(r"^(?=.*[0-9])(?=.*[!@#$%^&*.,])[a-zA-Z0-9!@#$%^&*.,]{6,16}$", password):
+        errors["password"] = "Contraseña debe contener al menos 6 caracteres, Una mayuscula, Una Minuscula, Un Numero y Un Caracter Especial"
+    if errors:
+        return jsonify(errors), 400
+    else:
+        new_user = User(**request_body)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(new_user.serialize()), 201
 
-#API DOCTOR GET
+#API DOCTOR GET, GET ID and POST
 @api.route('/mediGeeks/doctors', methods=['GET'])
 def get_doctor_table():
     doctor = Doctor.query.all()
     doctor = list(map(lambda p:p.serialize(),doctor))
     return jsonify(doctor), 200
+
+@api.route('/mediGeeks/doctors/<int:id>', methods=['GET'])
+def get_doctor_table_id(id):
+    doctor = Doctor.query.filter_by(id=id).first()
+    if doctor:
+        return jsonify(doctor.serialize()), 200
+    else:
+        return "Doctor no existe", 404
+
+@api.route('/mediGeeks/doctors', methods=['POST'])
+def add_new_doctor():
+    request_body = request.get_json()
+    name = request_body.get("name")
+    email = request_body.get("email")
+    rut = request_body.get("rut")
+    password = request_body.get("password")
+    errors = {}
+    if not name or not re.match(r"^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]+(?: [a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]+)*$", name):
+        errors["name"] = "Name should only contain letters, spaces and some accented characters"
+    if not rut or not re.match(r"\b[0-9|.]{1,10}\-[K|k|0-9]", rut):
+        errors["rut"] = "Rut no es valido"
+    if not email or not re.match(r"^(([^<>()[\],;:\s@']+(\.[^<>()[\],;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", email):
+        errors["email"] = "Invalid email address"
+    if not password or not re.match(r"^(?=.*[0-9])(?=.*[!@#$%^&*.,])[a-zA-Z0-9!@#$%^&*.,]{6,16}$", password):
+        errors["password"] = "Contraseña debe contener al menos 6 caracteres, Una mayuscula, Una Minuscula, Un Numero y Un Caracter Especial"
+    if errors:
+        return jsonify(errors), 400
+    else:
+        new_doctor = Doctor(**request_body)
+        db.session.add(new_doctor)
+        db.session.commit()
+        return jsonify(new_doctor.serialize()), 201
 
 #API CENTRO GET
 @api.route('/mediGeeks/centers', methods=['GET'])
@@ -49,14 +95,14 @@ def get_center_table():
     return jsonify(center), 200 
 
 #API PREVISION GET
-@api.route('/mediGeeks/prevision', methods=['GET'])
+@api.route('/mediGeeks/previsions', methods=['GET'])
 def get_prevision():
     prevision = Prevision.query.all()
     prevision = list(map(lambda p:p.serialize(),prevision))
     return jsonify(prevision), 200 
 
 #API speciality GET
-@api.route('/mediGeeks/speciality', methods=['GET'])
+@api.route('/mediGeeks/specialitys', methods=['GET'])
 def get_speciality():
     speciality = Speciality.query.all()
     speciality = list(map(lambda p:p.serialize(),speciality))
@@ -64,45 +110,6 @@ def get_speciality():
 
 
 
-#API USER PUT/ID
-@api.route('/mediGeeks/users/<user_id>', methods=['PUT'])
-def update_user_id(user_id):
-    
-    data = request.json 
-    for user in usersTable: 
-        if user["ID"] == user_id:
-            user = data
-            return jsonify(user), 200 
-
-    return "Usuario no existe", 404
 
 
-#API doctor GET/ID
-@api.route('/mediGeeks/doctors/<doctor_id>', methods=['GET'])
-def doctors_table_id(doctor_id):
 
-    print(doctor_id)
-    for doctor in doctorsTable:
-        if doctor["ID"] == doctor_id:
-            return jsonify(doctor),200
-    
-    return "Doctor no existe", 200 
-
-#API doctor POST
-@api.route('/mediGeeks/doctors', methods=['POST'])
-def add_new_doctor():
-    request_body = request.json
-    doctorsTable.append(request_body)  
-    return jsonify(doctorsTable), 200
-
-#API DOCTOR PUT/ID
-@api.route('/mediGeeks/doctors/<doctor_id>', methods=['PUT'])
-def update_doctor_id(doctor_id):
-    
-    data = request.json 
-    for doctor in doctorsTable: 
-        if doctor["ID"] == doctor_id:
-            user = data
-            return jsonify(doctor), 200 
-
-    return "Usuario no existe", 404
