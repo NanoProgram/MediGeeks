d490cc7ed3998597fa6b9879548f16b1c5341012
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-01-23 19:20:42 +0000

    casi listo ecriptado

--- src/api/models.py
+++ src/api/models.py
@@ -5,14 +5,15 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__='user'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(100), primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(120), unique=False, nullable=False)
     rut = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(260), unique=False, nullable=False)
     prevision_id = db.Column(db.Integer, db.ForeignKey('prevision.id'), nullable=False)
     verified = db.Column(db.Boolean(), default=False, nullable=False)
-
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
     def __repr__(self):
         return f'<User {self.email}>'
 
