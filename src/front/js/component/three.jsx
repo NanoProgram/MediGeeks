e316352461961053dd93db264cbec634fadcdb1b
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-01-25 03:08:16 +0000

    pruebas de estilos

--- src/front/js/component/three.jsx
+++ src/front/js/component/three.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "../../styles/three.css"
 export const Three = () => {
   return (
-    <div id="carouselExampleDark" class="carousel carousel-dark slide">
+    <div id="carouselExampleDark" class="carousel carousel-dark slide mx-auto" style={{width:'80%', height:'80%'}}>
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">holaaaaa</button>
       <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -12,22 +12,24 @@ export const Three = () => {
       <div class="carousel-item active" data-bs-interval="10000">
         <img src="https://www.vidatres.cl/wp-content/uploads/2016/11/alemana-la-dehesa-fachada.jpg" class="d-block w-100" alt="..."></img>
         <div class="carousel-caption d-none d-md-block">
-          <h5 >First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
+          <h2 style={{color:"white"}}>Clinica Alemana</h2>
+          
         </div>
       </div>
       <div class="carousel-item" data-bs-interval="2000">
-        <img src="https://www.vidatres.cl/wp-content/uploads/2016/11/alemana-la-dehesa-fachada.jpg" class="d-block w-100" alt="..."></img>
+        <img src="https://www.vidatres.cl/wp-content/uploads/2016/11/fachada-indisa.jpg" class="d-block w-100" alt="..."></img>
         <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
+          <h2 style={{color:"white"}}>Clinica Indisa</h2>
           <p>Some representative placeholder content for the second slide.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://www.vidatres.cl/wp-content/uploads/2016/11/alemana-la-dehesa-fachada.jpg" class="d-block w-100" alt="..."></img>
+        <img src="https://www.vidatres.cl/wp-content/uploads/2016/05/examenes_de_laboratorio.jpg" class="d-block w-100" alt="..."></img>
         <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
+          <h5 style={{color:"sky blue"}}>Examenes de Laboratorios</h5>
+
+
+          <p style={{color:"bsky blue"}}>La API de exámenes de laboratorio es una herramienta valiosa para cualquier sitio web que brinde servicios de salud en línea. Con ella, los usuarios pueden acceder a una amplia variedad de exámenes de laboratorio desde la comodidad de sus hogares, lo que les permite ahorrar tiempo y evitar desplazamientos innecesarios. La integración es fácil y la interfaz es intuitiva, lo que permite a los usuarios navegar con facilidad y obtener resultados de manera rápida y precisa. Además, la API ofrece una gran cantidad de opciones de personalización, lo que permite a los desarrolladores adaptarla a las necesidades específicas de su sitio web. En resumen, la API de exámenes de laboratorio es una excelente opción para aquellos que buscan ofrecer un servicio de salud en línea completo y de alta calidad a sus usuarios.</p>
         </div>
       </div>
     </div>
--- src/front/js/service/emailService.js
+++ src/front/js/service/emailService.js
@@ -9,7 +9,7 @@ console.log(params)
     from_name: "MediGeeks",
     to_email: params.to_email,
     to_name: params.to_name,
-    to_link: params.to_link
+    to_link: params.to_link,
   };
   emailjs.send(serviceId, templateId, templateParams, key).then(
     (result) => {
